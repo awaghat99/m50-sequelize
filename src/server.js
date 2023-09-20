fae251c04e0a1827c4694d371f05663d85234b3f
awaghat99 <awaghat99@gmail.com>
2023-09-20 11:48:35 +0100

    add Book model server.js

--- src/server.js
+++ src/server.js
@@ -1,9 +1,27 @@
+require("dotenv").config();
 const express = require("express");
 
-const port = 80;
+const port = process.env.PORT || 5001;
 
 const app = express();
 app.use(express.json());
+
+const { DataTypes } = require("sequelize");
+const connection = require("./db/connection");
+
+const Book = connection.define("Book", {
+  title: {
+    type: DataTypes.STRING,
+    unique: true,
+    allowNull: false,
+  },
+  author: {
+    type: DataTypes.STRING,
+  },
+  genre: {
+    type: DataTypes.STRING,
+  },
+});
 
 app.get("/health", (req, res) => {
   res.status(200).json({ message: "App is healthy" });
