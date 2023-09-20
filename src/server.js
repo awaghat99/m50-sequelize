72434c241d77d885555a9e43605b33016be6d956
awaghat99 <awaghat99@gmail.com>
2023-09-20 11:14:08 +0100

    add health route in server.js

--- src/server.js
+++ src/server.js
@@ -5,6 +5,10 @@ const port = 80;
 const app = express();
 app.use(express.json());
 
+app.get("/health", (req, res) => {
+  res.status(200).json({ message: "App is healthy" });
+});
+
 app.listen(port, () => {
   console.log(`App is listening on port ${port}`);
 });
