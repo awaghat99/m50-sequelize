5907ef2ccd50c8c371532789e313f9094da782f4
awaghat99 <awaghat99@gmail.com>
2023-09-22 11:42:24 +0100

    add getauthorbyparams route

--- src/authors/controllers.js
+++ src/authors/controllers.js
@@ -1,5 +1,6 @@
 const Author = require("./model");
 const Book = require("../books/model");
+const Genre = require("../genres/model");
 
 const addAuthor = async (req, res) => {
   try {
@@ -27,7 +28,31 @@ const getBooksByAuthor = async (req, res) => {
   } catch (error) {}
 };
 
+const getAuthorByParams = async (req, res) => {
+  try {
+    const author = await Author.findOne({
+      where: {
+        author: req.params.author,
+      },
+    });
+    const books = await Book.findAll({
+      where: { AuthorId: author.id },
+    });
+    const booksAndGenre = [];
+    for (let book of books) {
+      const genre = await Genre.findOne({
+        where: { id: book.GenreId },
+      });
+      booksAndGenre.push({ book: book, genre: genre });
+    }
+    res.status(200).json({ message: `found books by ${req.params.author}`, author: author, books: booksAndGenre });
+  } catch (error) {
+    res.status(400).json({ error: error, errorMessage: error.message });
+  }
+};
+
 module.exports = {
   addAuthor,
   getBooksByAuthor,
+  getAuthorByParams,
 };
--- src/authors/routes.js
+++ src/authors/routes.js
@@ -1,10 +1,12 @@
 const { Router } = require("express");
 const authorRouter = Router();
 
-const { addAuthor, getBooksByAuthor } = require("./controllers");
+const { addAuthor, getBooksByAuthor, getAuthorByParams } = require("./controllers");
 
 authorRouter.post("/addauthor", addAuthor);
 
-authorRouter.get("/getbooksbyauthor", getBooksByAuthor)
+authorRouter.get("/getbooksbyauthor", getBooksByAuthor);
+
+authorRouter.get("/getauthorbyparams/:author", getAuthorByParams);
 
 module.exports = authorRouter;
